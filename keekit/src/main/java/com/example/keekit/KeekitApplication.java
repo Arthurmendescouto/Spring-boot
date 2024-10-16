f295d84900b23273bc553a82215523eb9bd86c1e
ArthurMendes <153393682+Arthurmendescouto@users.noreply.github.com>
2024-10-15 21:09:06 -0300

    inicio

---
+++ keekit/src/main/java/com/example/keekit/KeekitApplication.java
@@ -0,0 +1,13 @@
+package com.example.keekit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeekitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeekitApplication.class, args);
+	}
+
+}
---
+++ keekit/src/test/java/com/example/keekit/KeekitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keekit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeekitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
